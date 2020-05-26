c0e25a2377569f3b087c65759abdce664b3d3cd7
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-26 14:11:04 -0700

    have user actions built.

--- my-app/src/actions/userActions.js
+++ my-app/src/actions/userActions.js
@@ -1,11 +1,14 @@
-import axios from 'axios';
+import { Auth } from "../utils/axiosWithAuth";
+
+// const URL = "https://watermyplants-core.herokuapp.com";
 
 export const CREATE_USER = 'CREATE_USER';
-export const GET_FRIENDS = 'GET_USER';
+export const GET_USER = 'GET_USER';
+export const DELETE_USER = 'DELETE_USER';
 export const ERROR = 'ERROR';
 
 export const createUser = user => {
-    const newUser = axios.post(`${URL}/create`, user);
+    const newUser = Auth().post("/create", user);
     return dispatch => {
       dispatch({ type: CREATING_USER });
       newUser
@@ -19,12 +22,28 @@ export const createUser = user => {
   };
 
   export const getUser = () => {
-    const user = axios.get(`${URL}/get`);
+    const user = Auth().get("/get");
     return dispatch => {
       dispatch({ type: GETTING_USER });
       user
         .then(response => {
           dispatch({ type: GET_USER, payload: response.data });
+        })
+        .catch(err => {
+          dispatch({ type: ERROR, payload: err });
+        });
+    };
+  };
+
+  export const deleteUser = id => {
+    const deletedUser = Auth().delete("/delete", {
+      data: { id }
+    });
+    return dispatch => {
+      dispatch({ type: DELETING_USER });
+      deletedUser
+        .then(({ data }) => {
+          dispatch({ type: DELETE_USER, payload: data });
         })
         .catch(err => {
           dispatch({ type: ERROR, payload: err });

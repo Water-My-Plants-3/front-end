532d4eb811786bde7dd85642e908e19c2f8bcd79
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-26 15:24:30 -0700

    have an error with the export of userReducer.

--- my-app/src/actions/userActions.js
+++ my-app/src/actions/userActions.js
@@ -1,52 +1,62 @@
-import { Auth } from "../utils/axiosWithAuth";
+import axios from "axios";
 
 // const URL = "https://watermyplants-core.herokuapp.com";
 
-export const CREATE_USER = 'CREATE_USER';
-export const GET_USER = 'GET_USER';
-export const DELETE_USER = 'DELETE_USER';
 export const ERROR = 'ERROR';
 
+export const CREATE_USER_START = 'CREATE_USER_START';
+export const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS';
+export const CREATE_USER_FAILED = 'CREATE_USER_FAILED';
+
+
 export const createUser = user => {
-    const newUser = Auth().post("/create", user);
+    const newUser = axios.post("/create", user);
     return dispatch => {
-      dispatch({ type: CREATING_USER });
-      newUser
-        .then(({ data }) => {
-          dispatch({ type: CREATE_USER, payload: data });
-        })
-        .catch(err => {
-          dispatch({ type: ERROR, payload: err });
-        });
+        dispatch({ type: CREATING_USER });
+        newUser
+            .then(({ data }) => {
+                dispatch({ type: CREATE_USER, payload: data });
+            })
+            .catch(err => {
+                dispatch({ type: CREATE_USER_FAILED, payload: err });
+            });
     };
-  };
+};
 
-  export const getUser = () => {
-    const user = Auth().get("/get");
+export const GET_USER_START = 'GET_USER_START';
+export const GET_USER_SUCCESS = 'GET_USER_SUCCESS';
+export const GET_USER_FAILED = 'GET_USER_FAILED';
+
+export const getUser = () => {
+    const user = axios.get("/get");
     return dispatch => {
-      dispatch({ type: GETTING_USER });
-      user
-        .then(response => {
-          dispatch({ type: GET_USER, payload: response.data });
-        })
-        .catch(err => {
-          dispatch({ type: ERROR, payload: err });
-        });
+        dispatch({ type: GETTING_USER });
+        user
+            .then(response => {
+                dispatch({ type: GET_USER, payload: response.data });
+            })
+            .catch(err => {
+                dispatch({ type: GET_USER_FAILED, payload: err });
+            });
     };
-  };
+};
 
-  export const deleteUser = id => {
-    const deletedUser = Auth().delete("/delete", {
-      data: { id }
+export const DELETE_USER_START = 'DELETE_USER_START';
+export const DELETE_USER_SUCCESS = 'DELETE_USER_SUCCESS';
+export const DELETE_USER_FAILED = 'DELETE_USER_FAILED';
+
+export const deleteUser = id => {
+    const deletedUser = axios.delete("/delete", {
+        data: { id }
     });
     return dispatch => {
-      dispatch({ type: DELETING_USER });
-      deletedUser
-        .then(({ data }) => {
-          dispatch({ type: DELETE_USER, payload: data });
-        })
-        .catch(err => {
-          dispatch({ type: ERROR, payload: err });
-        });
+        dispatch({ type: DELETING_USER });
+        deletedUser
+            .then(({ data }) => {
+                dispatch({ type: DELETE_USER, payload: data });
+            })
+            .catch(err => {
+                dispatch({ type: DELETE_USER_FAILED, payload: err });
+            });
     };
-  };
+};
---
+++ my-app/src/reducers/userReducer.js
@@ -0,0 +1,40 @@
+import {
+    CREATE_USER_START,
+    CREATE_USER_SUCCESS,
+    CREATE_USER_FAILED,
+
+    GET_USER_START,
+    GET_USER_SUCCESS,
+    GET_USER_FAILED,
+
+    DELETE_USER_START,
+    DELETE_USER_SUCCESS,
+    DELETE_USER_FAILED,
+
+    ERROR,
+
+} from "../actions/userActions";
+
+const intialState = {
+    creatingUser: false,
+    error: null,
+}
+
+export default function userReducer(state = intialState, action) {
+    switch (action.type) {
+        // user register
+        case CREATE_USER_START:
+            return {
+                ...state,
+                creatingUser: true,
+            }
+        case CREATE_USER_FAILED:
+            return {
+                ...state,
+                creatingUser: false,
+                error: action.payload.Error,
+            }
+        default:
+            return state
+    }
+};

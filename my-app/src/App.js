73d6616f4ea564132ed483c2b2776992630315fa
manuel <me@example.com>
2020-05-27 10:07:24 -0700

    moved form state to component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,43 +2,8 @@ import React, { useState, useEffect} from 'react';
 import './App.css';
 import SignUpForm from "./components/SignUp";
 import { Route, Switch } from "react-router-dom";
-import { initialFormValues } from "./constants/SignUpConstants";
 import { LoginForm } from './components/Login';
-import { initialLoginFormValues } from "./constants/LoginFormConstants";
 function App() {
-
-
-  // signup form state hooks ________________________________________________________
-  const [ signUpformValues, setSignUpformValues ] = useState(initialFormValues)
-  const [ signUpValid, setSignUpValid] = useState(false)
-  // signup form event handlers_________________________________________________________
-  const signUpOnChange = event => {
-    setSignUpformValues({...signUpformValues, [event.target.name]:event.target.value
-    })
-    if (false) {
-      setSignUpValid(true)
-    }}
-  const signUpOnSubmit = event => {event.preventDefault();console.log('you clicked submit');
-  }
-  // signup form side effects______________________________________________________________
-
-
-
-  // login form state hooks
-  const [ loginFormValues, setLoginFormValues ] = useState(initialLoginFormValues)
-  const [ LoginValid, setLoginValid] = useState(false)
-  //login form event handlers
-  const LoginOnChange = event => {
-    setSignUpformValues({...signUpformValues, [event.target.name]:event.target.value
-    })
-    if (false) {
-      setSignUpValid(true)
-    }}
-  const LoginOnSubmit = event => {event.preventDefault();console.log('you clicked submit');
-  }
-  //login form side effects
-
-
   return (
     <div className="App">
       <Switch>
@@ -48,10 +13,10 @@ function App() {
       </header>
       </Route>
       <Route path='/signup'>
-        <SignUpForm onSubmit={signUpOnSubmit} onChange={signUpOnChange} formValues={signUpformValues} valid={signUpValid}/>
+        <SignUpForm/>
       </Route>
       <Route path='/login'>
-        <LoginForm LoginOnChange={LoginOnChange} LoginOnSubmit={LoginOnSubmit} loginFormValues={loginFormValues} LoginValid={LoginValid}/>
+        <LoginForm />
       </Route>
       </Switch>
     </div>

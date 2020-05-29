8ee87b735d9505f87dc3e0d4fadd6d4cccbc8b24
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-29 11:30:55 -0700

    final working version for me

--- my-app/src/reducers/plantReducer.js
+++ my-app/src/reducers/plantReducer.js
@@ -1,6 +1,6 @@
 import { FETCH_PLANT_START, FETCH_PLANT_SUCCESS, FETCH_PLANT_FAIL } from '../actions/plantActions'
 import { POST_PLANT_START, POST_PLANT_SUCCESS, POST_PLANT_FAIL } from '../actions/plantActions'
-import { DELETE_PLANT_START, DELETE_PLANT_SUCCESS, DELETE_PLANT_FAIL } from '../actions/plantActions'
+// import { DELETE_PLANT_START, DELETE_PLANT_SUCCESS, DELETE_PLANT_FAIL } from '../actions/plantActions'
 
 
 const initialState = {
@@ -49,21 +49,21 @@ export const plantReducer = (state = initialState, action) => {
                 plantsFetching: false
             }
         //delete a plant 
-        case DELETE_PLANT_START:
-            return{
-                ...state,
-                plantsFetching: true
-            }
-        case DELETE_PLANT_SUCCESS:
-            return{
-                ...state,
-                plantsFetching: false
-            }
-        case DELETE_PLANT_FAIL:
-            return{
-                ...state,
-                error: action.payload
-            }
+        // case DELETE_PLANT_START:
+        //     return{
+        //         ...state,
+        //         plantsFetching: true
+        //     }
+        // case DELETE_PLANT_SUCCESS:
+        //     return{
+        //         ...state,
+        //         plantsFetching: false
+        //     }
+        // case DELETE_PLANT_FAIL:
+        //     return{
+        //         ...state,
+        //         error: action.payload
+        //     }
         default:
             return state
     }
